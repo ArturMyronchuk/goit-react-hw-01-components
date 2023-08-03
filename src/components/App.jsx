7db4b28a2fc8d9a48cfee5fce113a506f3600f3b
Artur Myronchuk <122921226+ArturMyronchuk@users.noreply.github.com>
2023-08-03 22:28:29 +0300

    dz1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,49 @@
-export const App = () => {
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101',
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
+// _____________________________________________
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../index.css';
+
+import Profile from './Profile/Profile.jsx';
+import Statistics from './Statistics/Statistics';
+import FriendList from './FriendList/Friendlist';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+import user from './Profile/user.json';
+import data from './Statistics/data.json';
+import friends from './FriendList/friends.json';
+import transactions from './TransactionHistory/transactions.json';
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="proffile">
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics className="stat" title="Upload stats" stats={data} />
+      {/* <Statistics stats={data} /> */}
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />;
     </div>
   );
 };
+
+ReactDOM.render(<App />, document.getElementById('root'));
+export default App;
